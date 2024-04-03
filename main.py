aef12069cd978982e6f3feb7420a48a231d1260a
Aryan Dhawan <aryansdhawan@gmail.com>
2024-04-02 17:16:55 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import math
+
+mean = 41.46153846
+standard = 12.64068757
+
+intervals = 50
+width = (2*standard)/intervals
+
+def function(val, mean=mean, standard=standard):
+    answer = (1/standard) * (1/math.sqrt(math.pi * 2))
+    
+    answer *= (math.e)**(-0.5 * (((val-mean)/standard)**2))
+    
+    return answer * width
+    
+
+rightRiemann = mean+standard
+
+values = []
+
+
+while intervals > 0:
+    values.append(rightRiemann)
+    rightRiemann -= width
+    intervals -= 1
+
+print(values)
+
+print(sum(list(map(function, values))))
+
